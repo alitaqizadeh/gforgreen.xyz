520bab7bab9e576557b891ea9d227d92a4fa6f55
Ali <alitaqizadeh@gmail.com>
2019-02-21 11:33:15 +0330

    Updates

---
+++ precache-manifest.3a07ea7573cf1e6431a0627fb10311f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gforgreen.xyz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1c8c96f91ff0d0117036",
+    "url": "/gforgreen.xyz/static/js/runtime~main.1c8c96f9.js"
+  },
+  {
+    "revision": "05e4404f385252b78768",
+    "url": "/gforgreen.xyz/static/js/main.05e4404f.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/gforgreen.xyz/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "05e4404f385252b78768",
+    "url": "/gforgreen.xyz/static/css/main.6233fe7d.chunk.css"
+  },
+  {
+    "revision": "9a464810a7a96eb3a3f00808476e264b",
+    "url": "/gforgreen.xyz/index.html"
+  }
+];
